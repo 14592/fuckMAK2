40271ce56ea899da3320c8294b6d2bb3e4eafade
14710 <nico.knueppel@nordakademie.de>
2018-09-11 08:46:38 +0200

    some changes added Shoppingcard

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -9,7 +9,22 @@ sap.ui.define([
             oRouter.navTo("products", {
                 path: oItem.getBindingContext("minibar").getPath().substr(1)
             });
+        },
+        onSCButtonPress: function (evt) {
+            var oButton = evt.getSource();
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.navTo("shoppingcard", {
+                path: oButton.getBindingContext("minibar").getPath().substr(1)
+            });
         }
-
+/*          jQuery.sap.require("sap.m.MessageToast");
+            sap.m.MessageToast.show(evt.getSource().getId() + " Pressed");*/
+        // onSCButtonClick: function (oEvent) {
+        //     var oButton = oEvent.getSource();
+        //     var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+        //     oRouter.navTo("shoppingcard", {
+        //         path: oButton.getBindingContext("minibar").getPath().substr(1)
+        //     })
+        // }
     })
 });
