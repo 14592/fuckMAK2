3aed5901d36b70a9e26a6908520294a215dce144
14592 <willi.prahl@nordakademie.de>
2018-09-13 14:32:53 +0200

    bestellen implementiert

--- webapp/controller/Shoppingcart.controller.js
+++ webapp/controller/Shoppingcart.controller.js
@@ -8,6 +8,18 @@ sap.ui.define([
          deleteSCItem: function (){
 
          },
+        onOrderButtonPress: function (){
+            var oModel = new sap.ui.model.odata.ODataModel('https://r41z.ucc.ovgu.de/sap/opu/odata/sap/ZVG_15D_54_MINIBAR_SRV_01/');
+
+            oModel.callFunction("/checkoutCart", {
+
+                method:"POST",
+
+            });
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.navTo("main", {}, true);
+
+        },
         //  onInit: function () {
         //      var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
         //      oRouter.getRoute("shoppingcart").attachPatternMatched(this._onObjectMatched,
